059bea39a95928e37d6ff070490923450e5c74e5
Ivan <yaivan13@gmail.com>
2019-11-10 03:55:47 +0200

    js: add eventlisteners and their behavior; html: add id's to the tasks counters, put onClick action in to js file; css: a little render fixes

--- Onix-Front-End-Test/script/script.js
+++ Onix-Front-End-Test/script/script.js
@@ -1,11 +1,52 @@
-const sidebar = document.getElementById('toggleBar');
+const CLOSE_THE_TASK = 'Are you sure you want to change the number of tasks?';
 
 window.onload = () => {
+
+  const sidebar = document.getElementById('sidebar');
+
+  //close sidebar if started window width less then 970px
   if (window.innerWidth < 970) { 
     sidebar.classList.add('hidden');
   };
+
+
+  
+  const toggleButton = document.getElementById('toggleButton');
+
+  /*Event: open/close sidebar menu */
+  toggleButton.addEventListener('click', () => { sidebar.classList.toggle("hidden"); });
+  
+
+  const imagesArr = document.querySelectorAll('.item__images > div');
+  const sidemenuNotifications = document.querySelector('.menu__notifications');
+
+  /*Event: change notification counter to the image index number*/
+  imagesArr.forEach( (element, index) => {
+    element.addEventListener('click', () => {
+      sidemenuNotifications.innerHTML = index;
+   });
+  });
+
+
+  const tasksOpen = document.getElementById('tasksOpen');
+  const tasksClosed = document.getElementById('tasksClosed');
+
+  /*Event: ask user to close a task*/
+  tasksOpen.addEventListener('click', closeTheTask);
+
+
+  /* Change tasks counters */
+  function closeTheTask() {
+    if(confirm(CLOSE_THE_TASK) ) {
+      console.log(tasksClosed.innerHTML);
+      
+      if (tasksOpen.innerHTML > 0) {
+        tasksOpen.innerHTML--;
+      }
+      tasksClosed.innerHTML++;
+    }
+  }
 }
 
-function toggle() {
-  sidebar.classList.toggle("hidden");
-}
+
+
