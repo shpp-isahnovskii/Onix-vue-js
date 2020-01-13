14d394cf1a45971c9a3e3bd270860b8be7d002ac
Ivan <yaivan13@gmail.com>
2020-01-13 15:30:44 +0200

    stuck on making kanban table, some test code here

--- Onix-Front-End-Test/src/components/navigation/kanban.vue
+++ Onix-Front-End-Test/src/components/navigation/kanban.vue
@@ -1,13 +1,10 @@
 <template lang="pug">
   section
     h3 Kanban
-    div {{tasks}}
-    div {{a}}
     table
-      thead
-        th(v-for='(header, index) in taskStatuses' :key='index') {{header}}
-
-
+      tr(v-for='(element, i) in parsedData' :key='i') 
+        td {{i}}
+        td(v-for='(task, n) in element' :key='n') {{task}}
 </template>
 
 <script lang="ts">
@@ -19,29 +16,66 @@
   @Component
   export default class Kanban extends Vue {
     tasks: TasksInterface;
-    a: string;
+    parsedData: any;
     taskStatuses: string[];
     constructor() {
       super();
       this.tasks = {};
-      this.a ='b';
+      this.parsedData = {};
       this.taskStatuses = ['todo', 'inprogress', 'done'];
       this.tasks = {
-      'task example': 
-      {
-        '7.00AM': { text: 'Do something..', status: 'todo' },
-      },
-    };
+        'wake up': 
+        {
+          '7.00AM': { text: 'Making bed.', status: 'todo' },
+          '7.05AM': { text: 'Washing face.', status: 'todo' },
+          '7.10AM': { text: 'Drinking a pint of lemon water.', status: 'todo' },
+          '7.15AM': { text: 'Maging breakfast', status: 'todo' },
+          '7.45AM': { text: 'Reviewing my goals.', status: 'todo' },
+          '7.50AM': { text: 'Writing down two to four important tasks for the day.', status: 'todo' }
+        },
+        'go to the work': 
+        {
+          '8.00AM': { text: 'Suit up.', status: 'todo' },
+          '8.10AM': { text: 'Go out.', status: 'todo' },
+          '8.15AM': { text: 'Driving to the office.', status: 'todo' },
+          '8.45AM': { text: 'Talk to the manager.', status: 'todo' },
+          '8.50AM': { text: 'Planning work day.', status: 'todo' },
+          '9.00AM': { text: 'Work hard or easy.', status: 'todo' }
+        },
+      };
     }
     created() {
+      this.parsedData = this.dataParsing();
       //EventBusTasks.$emit('get-tasks');
       EventBusTasks.$on('tasks-refresh', (e: TasksInterface) => {
       this.tasks = e;
-      //this.a = 'a'
       //eslint-disable-next-line no-console
-      console.log(Object.keys(e));
+      //console.log(Object.keys(e));
       EventBusTasks.$off('tasks-refresh');
       });
+    }
+    dataParsing() {
+      let newData: { 
+        todo: string[],
+        inprogress: string[],
+        done: string[]
+        } 
+      = {
+        todo : [],
+        inprogress : [],
+        done : []
+      };
+      Object.entries(this.tasks).forEach( element => {
+        Object.entries(element[1]).forEach( el => {
+        let status = el[1].status;
+        // eslint-disable-next-line no-console
+        console.log(status);
+
+          newData.todo.push(el[0] + ' ' + el[1].text);
+      });
+
+      });
+      return newData;
     }
   }
 </script>
