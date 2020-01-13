76fe003d3914bb89832ecee4989a00d6b7ec2dea
Ivan <yaivan13@gmail.com>
2020-01-13 23:03:48 +0200

    have [DEFAULT]: WARN : Using DEFAULT root logger bug in the console, have bug with table rows. Done refactoring in kanban tasks data

--- Onix-Front-End-Test/src/components/navigation/kanban.vue
+++ Onix-Front-End-Test/src/components/navigation/kanban.vue
@@ -2,9 +2,17 @@
   section
     h3 Kanban
     table
-      tr(v-for='(element, i) in parsedData' :key='i') 
-        td {{i}}
-        td(v-for='(task, n) in element' :key='n') {{task}}
+      thead
+        tr
+          th(v-for='(element, i) in taskStatus' :key='i') {{element}}
+      tbody
+        tr(v-for='(element, i) in tasks' :key='i') 
+          td(v-if="element.task.status === taskStatus[0]") {{`${element.task.time}: ${element.task.description}`}}
+          td(v-else) {{''}}
+          td(v-if="element.task.status === taskStatus[1]") {{`${element.task.time}: ${element.task.description}`}}
+          td(v-else) {{''}}
+          td(v-if="element.task.status === taskStatus[2]") {{`${element.task.time}: ${element.task.description}`}}
+          td(v-else) {{''}}
 </template>
 
 <script lang="ts">
@@ -15,22 +23,18 @@
   
   @Component
   export default class Kanban extends Vue {
-    tasks: any;
-    parsedData: any;
-    taskStatuses: string[];
+    tasks: any[];
+    taskStatus: string[];
     constructor() {
       super();
-      this.tasks = {};
-      this.parsedData = {};
-      this.taskStatuses = ['todo', 'inprogress', 'done'];
+      this.taskStatus = ['todo', 'inprogress', 'done'];
       this.tasks = [
-        { title: 'wake up', task: { description: 'Making bed.', time: '7.00AM', status: 'todo'}},
-        { title: 'wake up', task: { description: 'Washing face.', time: '7.05AM', status: 'todo'}},
+        { title: 'wake up', task: { description: 'Making bed.', time: '7.00AM', status: 'done'}},
+        { title: 'wake up', task: { description: 'Washing face.', time: '7.05AM', status: 'inprogress'}},
         { title: 'wake up', task: { description: 'Drinking a pint of lemon water.', time: '7.10AM', status: 'todo'}},
         { title: 'wake up', task: { description: 'Making breakfast.', time: '7.15AM', status: 'todo'}},
         { title: 'wake up', task: { description: 'Reviewing my goals.', time: '7.45AM', status: 'todo'}},
         { title: 'wake up', task: { description: 'Writing down two to four important tasks for the day.', time: '7.50AM', status: 'todo'}},
-
         { title: 'go to the work', task: { description: 'Suit up.', time: '8.00AM', status: 'todo'}},
         { title: 'go to the work', task: { description: 'Go out.', time: '8.10AM', status: 'todo'}},
         { title: 'go to the work', task: { description: 'Driving to the office.', time: '8.15AM', status: 'todo'}},
@@ -42,11 +46,19 @@
     created() {
       //EventBusTasks.$emit('get-tasks');
       EventBusTasks.$on('tasks-refresh', (e: TasksInterface) => {
-      this.tasks = e;
-      //eslint-disable-next-line no-console
-      //console.log(Object.keys(e));
+      //this.tasks = e;
       EventBusTasks.$off('tasks-refresh');
       });
     }
   }
 </script>
+
+<style lang="scss">
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    width: 33%;
+    border: solid silver 1px;
+  }
+</style>
