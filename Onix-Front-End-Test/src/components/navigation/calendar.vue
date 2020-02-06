02e162cfcdac54cd38c0d0d9e658276b843964d4
Ivan <yaivan13@gmail.com>
2020-02-06 18:54:41 +0200

    made simple calendar

--- Onix-Front-End-Test/src/components/navigation/calendar.vue
+++ Onix-Front-End-Test/src/components/navigation/calendar.vue
@@ -3,13 +3,17 @@
     h3 Calendar
     .calendar_wrapper
       div {{year}}
-      div {{fullMonth}}
+      div {{fullMonthName}}
       table
         tr
-          th(v-for="(n, i) in week" v-bind:key="i") {{n}}
+          //- header
+          th(v-for="(day, i) in daysInWeek" v-bind:key="i") {{day}}
         tr
-          td(v-for="(i) in lastDay" v-bind:key="i") {{i}}
-
+          //- body
+          tr(v-for="(n) in rows" v-bind:key="n")
+            td(v-for="day in daysInWeek.length")
+              span(v-if="dayCounter(n, day) <=0 || dayCounter(n, day) >=lastDay") -
+              span(v-else) {{dayCounter(n, day)}}
         
 </template>
 
@@ -20,13 +24,15 @@
 
   @Component
   export default class Calendar extends Vue {
-    week: string[];
+    daysInWeek: string[];
     timeNow: Date;
+    counter: number;
 
     constructor() {
       super();
-      this.week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+      this.daysInWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
       this.timeNow = new Date();
+      this.counter = 0;
     }
 
     get tasks() : TasksInterface[] {
@@ -35,23 +41,32 @@
     created() {
       this.$store.dispatch('loadTasks', dataTasks);
     }
-
     /* return current year value */
     get year(): number {
       return this.timeNow.getFullYear();
     }
-    /* return current month value */
+    /* return current month value, used for programming */
     get shortMonthName(): number {
       return this.timeNow.getMonth();
     }
-    get firstDay(): number {
+    /* means day of the week */
+    get startDay(): number {
       return (new Date(this.year, this.shortMonthName, 1)).getDay();
     }
+    /* last day, means number */
     get lastDay(): number {
       return (new Date(this.year, this.shortMonthName + 1, 0)).getDate();
     }
+    /* this month name user see */
     get fullMonthName(): string {
       return this.timeNow.toLocaleString('default', { month: 'long' });
+    }
+    /* get amount of rows what I need for the table */
+    get rows(): number {
+      return Math.ceil( (this.startDay - 1 + this.lastDay) / 7 );
+    }
+    dayCounter(increment: number, thisDay: number ): number {
+      return ( 7 * (increment - 1) + thisDay - this.startDay )
     }
   }
 </script>
