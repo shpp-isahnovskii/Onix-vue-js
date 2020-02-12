3b48fb15800f2480abe1ecc021ce22007371bb74
Ivan <yaivan13@gmail.com>
2020-02-12 21:36:19 +0200

    add axios dependency, not new code here

--- Onix-Front-End-Test/src/store/modules/taskStore.ts
+++ Onix-Front-End-Test/src/store/modules/taskStore.ts
@@ -1,5 +1,7 @@
 import { createModule, mutation, action, extractVuexModule } from "vuex-class-component";
-import { TasksInterface } from "@/interfaces/TasksInterface.ts"
+import { TasksInterface } from "@/interfaces/TasksInterface.ts";
+import axios from "axios"
+
 
 const VuexModule = createModule({
   namespaced: "tasks",
@@ -10,30 +12,17 @@ export class TaskStore extends VuexModule {
 
   private taskStatuses = ['todo', 'inprogress', 'done'];
 
-  private tasksData : TasksInterface[] = [
-    {id: 0, title: 'task1', description: 'Making bed.', date: '2020-01-27T07:00', status: 'done'},
-    {id: 1, title: 'task2', description: 'Washing face.', date: '2020-01-27T07:05', status: 'inprogress'},
-    {id: 2, title: 'task3', description: 'Drinking a pint of lemon water.', date: '2020-01-27T07:10', status: 'todo'},
-    {id: 3, title: 'task4', description: 'Making breakfast.', date: '2020-01-28T07:15', status: 'todo'},
-    {id: 4, title: 'task5', description: 'Reviewing my goals.', date: '2020-01-28T07:45', status: 'todo'},
-    {id: 5, title: 'task7', description: 'Writing down two to four important tasks for the day.', date: '2020-02-01T07:50', status: 'todo'},
-    {id: 6, title: 'task8', description: 'Suit up.', date: '2020-02-02T08:00', status: 'todo'},
-    {id: 7, title: 'task9', description: 'Go out.', date: '2020-02-02T08:10', status: 'todo'},
-    {id: 8, title: 'task10', description: 'Driving to the office.', date: '2020-02-18T08:15', status: 'todo'},
-    {id: 9, title: 'task11', description: 'Talk to the manager.', date: '2020-02-18T08:45', status: 'todo'},
-    {id: 10, title: 'task12', description: 'Planing work day.', date: '2020-03-01T08:50', status: 'todo'},
-    {id: 11, title: 'task13', description: 'Work hard or easy.', date: '2020-03-29T09:00', status: 'todo'},
-  ];
+  private tasksData : TasksInterface[] = [];
+
+  @action async fetchTasks(tasks : TasksInterface[]) {
+    this.tasksData = tasks;
+  }
 
   @mutation deleteTask(index: number) {
     this.tasksData.splice(index, 1);
   }
 
   @mutation setTaskStatus(task : {id: number, status: string}) {
-
-    // eslint-disable-next-line no-console
-    console.log(task.id + " " + task.status);
-    
     this.tasksData[task.id].status = task.status;
   }
 
